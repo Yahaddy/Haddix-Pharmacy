a166895d5efe74c1e59f4aa0e2b8934172ef6140
ya haddy kasseh <yahaddykasseh@gmail.com>
2022-10-15 13:19:49 +0100

    server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,31 @@
 const express = require('express');
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Peace' });
-});
-
-module.exports = app;
+ const app = express();
+ 
+ const connect = require('./modules/connect');
+ const routes = require('./routes/router');
+ 
+ 
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: true }));
+ 
+ 
+ // Connect to Database
+ void (async () => {
+  try {
+   await connect();
+   console.log('connected to database');
+  } catch (error) {
+   console.log('error connecting to database:', error.message);
+  }
+ })();
+ 
+ 
+ app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Peace' });
+ });
+ 
+ 
+ app.use('/api', routes);
+ 
+ 
+ module.exports = app;
