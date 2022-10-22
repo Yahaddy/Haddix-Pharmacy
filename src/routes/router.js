5778b5cda6650e2358044317bc6387c44240b71d
ya haddy kasseh <yahaddykasseh@gmail.com>
2022-10-22 13:39:47 +0100

    part 9-12

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,16 @@
 const express = require('express');
- const router = express.Router();
- 
- const { createUser } = require('../controllers/user');
+const router = express.Router();
  
  
- // User route
- router.post('/user', createUser);
- 
- 
- module.exports = router;
+const { createUser, getAllUsers, getOneUser, updateOne, deleteOne } = require('../controllers/user');
+
+// User route
+router.post('/user', createUser);
+router.get('/user', getAllUsers);
+router.get('/user/:id', getOneUser);
+router.put('/user/:id', updateOne);
+router.delete('/user/:id', deleteOne);
+
+
+
+module.exports = router;
