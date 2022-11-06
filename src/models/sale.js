993e09d73d804c085e34298a03ae45e93aaaf8f3
ya haddy kasseh <yahaddykasseh@gmail.com>
2022-11-06 15:15:41 +0000

    sales attributes

---
+++ src/models/sale.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+ 
+ const saleSchema = new mongoose.Schema(
+  {
+   description: {
+    type: String,
+    required: true,
+    trim: true,
+ },
+
+   Quantity: {
+    type: Number,
+    required: true,
+    trim: true,
+ },
+
+   price: {
+    type: Number,
+    required: true,
+    trim: true,
+ },
+
+ user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  }
+},
+  { timestamps: true }
+ )
+ 
+ 
+ saleSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+ 
+ 
+ const sale = mongoose.model('sale', saleSchema)
+ 
+ module.exports = sale
