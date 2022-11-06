7baa59d595e8411ebba633421cb4b8860c38b2ad
ya haddy kasseh <yahaddykasseh@gmail.com>
2022-11-06 15:17:09 +0000

    exporting sales

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,10 @@
 const express = require('express');
+
 const router = express.Router();
  
  
 const { createUser, getAllUsers, getOneUser, updateOne, deleteOne } = require('../controllers/user');
+const { createSale, getAllSale, getOneSale,} = require('../controllers/sales');
 
 // User route
 router.post('/user', createUser);
@@ -11,6 +13,13 @@ router.get('/user/:id', getOneUser);
 router.put('/user/:id', updateOne);
 router.delete('/user/:id', deleteOne);
 
+
+//sales route
+router.post('/sale', createSale);
+router.get('/sale', getAllSale);
+router.get('/sale/:id', getOneSale);
+router.put('/sale/:id', updateOne);
+router.delete('/user/:id', deleteOne);
 
 
 module.exports = router;
